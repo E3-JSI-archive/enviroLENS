21b26c49bb8059195c8d28b05c412efa778a13a0
ZivaUrbancic <ziva.urbancic@gmail.com>
2019-08-28 09:16:41 +0200

    Added module document_similarity

---
+++ word-embeddings/modules/library/document_similarity.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class DocumentSimilarity(embedding):
+    """Given a document embedding, provides tools for analysis of document similarity.
+
+    Args:
+        embedding (numpy.ndarray): matrix of document embeddings
+
+    """
+
+    def __init__(self, embedding):
+        self.embedding = embedding
+
+    def euclid_similarity(self, id1, id2):
+        """Calculate the euclid similarity between two embeddings.
+
+        Args:
+            id1 (int): index of the embedding of the first document in the embedding matrix
+            id2 (int): index of the embedding of the second document in the embedding matrix
+
+        Returns:
+            numpy.float32: euclid distance between embeddings of documents with given indices
+
+        """
+        return np.linalg.norm(self.embedding[id1, :] - self.embedding[id2, :])
+
+    def k_nearest_neighbors(self, index, k, similarity):
+        """Get the k documents with embeddings nerest to the document embedding we chose.
+
+        Args:
+            index (int): index of thw chosen document, whose neighbors we are trying to find
+            k (int): number of neighbors we want to find
+            similarity (function): metric function that we want to use for computing the distance between documents
+
+        Returns:
+            list: a list of k indices of the k documents whose embeddings are closest to the chosen embedding in the
+            specified metric
+        """
+
+        # calculate the similarities and revert it
+        sims = [similarity(embedding, d) for d in de]
+
+        # sort and get the corresponding indices
+        indices = []
+        for c, index in enumerate(np.argsort(sims)):
+            if c == k:
+                break
+            indices.append(index)
+
+        # return indices of the neighbors
+        return indices
